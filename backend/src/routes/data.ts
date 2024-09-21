a1811c3ed95a2dd57e0147ccd2b2c2c3cf046aae
jainam-b <jainambagrecha16@gmail.com>
2024-09-21 16:21:06 +0530

    updates

--- backend/src/routes/data.ts
+++ backend/src/routes/data.ts
@@ -13,7 +13,8 @@ export interface SentMessage {
 
 export const contacts: Contact[] = [
   { id: 1, firstName: 'John', lastName: 'Doe', phone: '+919157228672' },
-  { id: 2, firstName: 'Jane', lastName: 'Smith', phone: '+919810153261' }
+  { id: 2, firstName: 'Jane', lastName: 'Smith', phone: '+919426678968' },
+  { id: 3, firstName: 'Testing', lastName: 'Number', phone: '+919810153260' }
 ];
 
 export const messages: SentMessage[] = [];
